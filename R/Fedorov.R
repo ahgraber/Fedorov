8b1a25741e05c5571d78048d485d29870967b2a2
aarmstrong32 <33763995+aarmstrong32@users.noreply.github.com>
2020-02-29 11:22:40 -0500

    Update Fedorov.R

--- R/Fedorov.R
+++ R/Fedorov.R
@@ -118,6 +118,40 @@ update_obj <- function(D, i, j_row, lambda, det, dvar) {
   return( (det*(1+dvar)-new_p) / (det-old_p) - 1 )
 }
 
+update_chol <- function(L, x) {
+  n <- length(x)
+  for (k in 1:n) {
+    r <- sqrt(L[k, k]^2 + x[k]^2)
+    c <- r / L[k, k]
+    s <- x[k] / L[k, k]
+    L[k, k] <- r
+    if (k < n) {
+      L[(k+1):n, k] <- (L[(k+1):n, k] + s * x[(k+1):n]) / c;
+      x[(k+1):n] <- c * x[(k+1):n] - s * L[(k+1):n, k];
+    }
+  }
+  return(L)
+}
+
+downdate_chol <- function(L, x) {
+  n <- length(x)
+  for (k in 1:n) {
+    r <- sqrt(L[k, k]^2 - x[k]^2)
+    c <- r / L[k, k]
+    s <- x[k] / L[k, k]
+    L[k, k] <- r
+    if (k < n) {
+      L[(k+1):n, k] <- (L[(k+1):n, k] - s * x[(k+1):n]) / c;
+      x[(k+1):n] <- c * x[(k+1):n] - s * L[(k+1):n, k];
+    }
+  }
+  return(L)
+}
+
+det_chol <- function(L) {
+  return(prod(diag(L))^2)
+}
+
 #-- Fedorov --------------------------
 fedorov <- function(dm, candidate_set, n, lambda=0) {
   # fedorov algorithm find d-optimal design
